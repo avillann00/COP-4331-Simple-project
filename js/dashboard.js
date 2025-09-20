fc6ef02f040462da68efb9ea1edf0e28312671fe
AustinVillanueva <avillann00@gmail.com>
2025-09-20 20:01:59 +0000

    feat(Contacts): Implement contact deletion
    features:
    - Delete button
    - Corresponding js
    - Styles for button
    - Message box in dashboard for errors

--- js/dashboard.js
+++ js/dashboard.js
@@ -9,12 +9,15 @@ document.getElementById('searchBox').addEventListener('input', function(e){
   e.preventDefault()
 
   search = document.getElementById('search').value
+  document.getElementById('message').textContent = ''
 
   if(search.trim() === ''){
     document.getElementById('first-name-box').textContent = ''
     document.getElementById('last-name-box').textContent = ''
     document.getElementById('email-box').textContent = ''
     document.getElementById('phone-box').textContent = ''
+    document.getElementById('delete-box').textContent = ''
+    document.getElementById('message').textContent = ''
 
     return
   }
@@ -34,11 +37,13 @@ document.getElementById('searchBox').addEventListener('input', function(e){
       const lastNameBox = document.getElementById('last-name-box')
       const emailBox = document.getElementById('email-box')
       const phoneBox = document.getElementById('phone-box')
+      const deleteBox = document.getElementById('delete-box')
 
       firstNameBox.textContent = ''
       lastNameBox.textContent = ''
       emailBox.textContent = ''
       phoneBox.textContent = ''
+      deleteBox.textContent = ''
 
       data.results.forEach(contact => {
         // create result divs
@@ -46,18 +51,47 @@ document.getElementById('searchBox').addEventListener('input', function(e){
         const lastNameDiv = document.createElement('div')
         const emailDiv = document.createElement('div')
         const phoneDiv = document.createElement('div')
+        const deleteButton = document.createElement('button')
 
         // add corresponding info
         firstNameDiv.textContent = contact.firstName
         lastNameDiv.textContent = contact.lastName
         emailDiv.textContent = contact.email
         phoneDiv.textContent = contact.phone
+        deleteButton.textContent = 'X'
+
+        deleteButton.classList.add('delete-btn')
+        // add the delete functionalify
+        deleteButton.addEventListener('click', () => {
+          fetch('/LAMPAPI/RemoveContact.php', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ firstName: contact.firstName, lastName: contact.lastName, phone: contact.phone, email: contact.email })
+          })
+          .then(response => response.json())
+          .then(data => {
+            if(!data.error){
+              firstNameDiv.remove()
+              lastNameDiv.remove()
+              emailDiv.remove()
+              phoneDiv.remove()
+              deleteButton.remove()
+            }
+            else{
+              document.getElementById('message').textContent = data.error
+            }
+          })
+          .catch(error => console.error('Error deleting contact: ', error))
+        })
 
         // add divs to thier result boxes
         firstNameBox.appendChild(firstNameDiv)
         lastNameBox.appendChild(lastNameDiv)
         emailBox.appendChild(emailDiv)
         phoneBox.appendChild(phoneDiv)
+        deleteBox.appendChild(deleteButton)
       })
     }
     else{
